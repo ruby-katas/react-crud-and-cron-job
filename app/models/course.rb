ca834f037a40306842bfa1e3ad995ef69e064554
pwalvarado <pwalvarado@gmail.com>
2018-08-04 17:25:19 -0500

    add relations to the models

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,9 @@
 class Course < ApplicationRecord
-  belongs_to :instructor
+  belongs_to :instructor, class_name: 'User', foreign_key: "user_id"
+
+  has_many :course_users
+  has_many :students, through: :course_users
+
+  has_many :course_books
+  has_many :books, through: :course_books
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
     enum role: { student: 0, instructor: 1 }
+
+    has_many :reading_times
+    has_many :course_books
+    has_many :books, through: :course_books
+    has_many :course_users
+    has_many :courses, through: :course_users
 end
