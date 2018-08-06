d9aa38f1de15a93110b101ea652660373a472369
pwalvarado <pwalvarado@gmail.com>
2018-08-06 14:21:17 -0500

    minimal styling (inline)

--- app/javascript/packs/students_react.jsx
+++ app/javascript/packs/students_react.jsx
@@ -1,14 +1,37 @@
 import React from "react";
 
 const Students = (props) => {
-  console.log('props   p3t', props)
+  // console.log('props', props)
+
+  const divTable = {
+    display:  'table',
+    width: 'auto',
+    'background-color': '#eee',
+    border: '1px solid  #ccc',
+    'border-spacing': '5px', /*cellspacing:poor IE support for  this*/
+     /* border-collapse:separate;*/
+  }
+
+  const divRow = {
+     display:'table-row',
+     width: 'auto',
+  }
+
+  const divCell = {
+      float: 'left', /*fix for  buggy browsers*/
+      display: 'table-column',
+      width: '200px',
+      padding: '5px',
+      border: '1px',
+      'background-color': '#fff',
+  }
 
   var students = props.students.map((student) => {
     return(
-      <div id={"student-" + student.id} class="row">
-        <div>{student.name}</div>
-        <div>{student.email}</div>
-        <div>{student.reading_time}</div>
+      <div id={"student-" + student.id} style={divRow}>
+        <div style={divCell}>{student.name}</div>
+        <div style={divCell}>{student.email}</div>
+        <div style={divCell}>{student.reading_time}</div>
       </div>
     )
   })
@@ -16,7 +39,17 @@ const Students = (props) => {
   var students
 
   return(
-    <div>{students}</div>
+    <div>
+      <h2>Students</h2>
+      <div style={divTable}>
+      <div style={divRow}>
+        <div style={divCell}><strong>Student</strong></div>
+        <div style={divCell}><strong>Email</strong></div>
+        <div style={divCell}><strong>Reading time [mins]</strong></div>
+      </div>
+      {students}
+      </div>
+    </div>
   )
 
 };
