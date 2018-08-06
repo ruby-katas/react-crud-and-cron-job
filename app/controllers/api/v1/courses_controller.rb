bebe1556b274a285039bd65491e5e1576bac5d0a
pwalvarado <pwalvarado@gmail.com>
2018-08-06 13:12:13 -0500

    add join to include reading time at course users

--- app/controllers/api/v1/courses_controller.rb
+++ app/controllers/api/v1/courses_controller.rb
@@ -1,5 +1,12 @@
 class Api::V1::CoursesController < ApplicationController
   def index
-    @course = Course.first
+    # TODO improve moving logic to a ruby object
+    @course = Course.includes(:books, :instructor, :students).first
+
+    @students = @course.students
+      .joins(:reading_times)
+      .where(reading_times: {book_id: @course.books.first})
+      .select('users.id, users.name, users.email, reading_times.reading_time as reading_time')
+      .all
   end
 end
--- app/views/api/v1/courses/index.json.jbuilder
+++ app/views/api/v1/courses/index.json.jbuilder
@@ -1,1 +1,7 @@
 json.extract! @course, :id, :name, :instructor
+
+json.book do
+    json.extract! @course.books.first, :id, :title
+end
+
+json.students @students, :id, :name, :email, :reading_time
